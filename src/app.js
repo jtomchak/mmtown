b2d01ec3da4782420d0b34674186f43b94623c41
Jesse Tomchak <jtomchak@gmail.com>
2018-03-27 16:06:48 -0700

    fetched posters and displayed as cards

--- src/app.js
+++ src/app.js
@@ -1,30 +1,47 @@
+import "jquery";
 import "./style.scss";
 
-//Create 4 Boxes, labeled "Box 1 - 4" using only javascript! NO HTML
-//And yes you probably want to loop over some sort of collection or Array!!!!
-let containerDiv = document.createElement("div");
-let rowDiv = document.createElement("div");
-containerDiv.setAttribute("class", "container");
-rowDiv.setAttribute("class", "row");
+$(document).ready(function() {
+  //Create a div with an img tag in it, and for each movie object
+  //assign the poster_path to the 'src' of the image tag
 
-for (let i = 0; i < 4; i++) {
-  let columnDiv = document.createElement("div");
-  let boxDiv = document.createElement("div");
-  let headTag = document.createElement("h3");
-  let p1 = document.createElement("p");
-  let p2 = document.createElement("p");
-  p1.innerHTML = "Lorem ipsum dolor..";
-  p2.innerHTML = "Ut enim ad..";
+  //create a function to fetch the movies
+  function fetchMovies() {
+    $.ajax({
+      url:
+        "https://api.themoviedb.org/3/search/movie?api_key=2434d246ec60c162a86db597467ef4ed&language=en-US&query=princess&include_adult=false&sort_by=created_at&page=1",
+      success: function(payload) {
+        console.log(payload);
+        //Call a function and pass that array to it?
+        //that function will iterate over the array, creating a div with
+        //child img tag, the src of which will be the post_path
+        createPosters(payload.results);
+      }
+    });
+  }
 
-  columnDiv.setAttribute("class", "col-md-4");
-  boxDiv.setAttribute("class", "box-bg");
-  headTag.innerHTML = "BOX " + (i + 1);
+  let containerDiv = $("<div>").attr("class", "container");
+  let rowDiv = $("<div>").attr("class", "row");
 
-  boxDiv.append(headTag, p1, p2);
+  //Create our function to build and display the posters
+  function createPosters(moviesList) {
+    moviesList.forEach(function(movie) {
+      let movieColDiv = $("<div>").attr("class", "card col-md-4");
+      let movieImage = $("<img>")
+        .attr("src", "https://image.tmdb.org/t/p/w500/" + movie.poster_path)
+        .attr("class", "card-img-top");
 
-  columnDiv.appendChild(boxDiv);
-  rowDiv.append(columnDiv);
-}
+      let posterBody = $("<div>")
+        .attr("class", "card-body")
+        .append($("<h5>").html(movie.title));
 
-containerDiv.append(rowDiv);
-document.body.appendChild(containerDiv);
+      movieColDiv.append(movieImage).append(posterBody);
+      rowDiv.append(movieColDiv);
+    });
+
+    rowDiv.appendTo(containerDiv);
+    containerDiv.appendTo("body");
+  }
+
+  fetchMovies();
+});
